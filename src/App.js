68ffed392ee268b71f81a88f541e38b2884cc160
ritin0204 <ritintiwari417@gmail.com>
2023-03-01 21:52:41 +0530

    completed application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+const changeTheme = (e) => {
+  const theme = process.env.PUBLIC_URL+"/styles/theme"+e.target.id+"vars.css";
+  const themeLink = document.getElementById("stylesheet");
+  themeLink.href = theme;
+  const activeTheme = document.querySelector(".active-theme");
+  activeTheme.classList.remove("active-theme");
+  e.target.classList.add("active-theme");
+}
 
 function App() {
+  const [result, setResult] = useState("0");
+  useEffect(() => {
+    const resultEle = document.querySelector(".result-div");
+    if (result.length > 12) {
+      resultEle.style.fontSize = "1.5rem";
+    } else if (result.length > 8) {
+      resultEle.style.fontSize = "2rem";
+    }
+    else {
+      resultEle.style.fontSize = "2.5rem";
+    }
+    let formatted = new Intl.NumberFormat('en-US', { maximumFractionDigits: 8 }).format(result);
+    if (formatted === "NaN" || result[result.length-1] === ".") {
+      formatted = result;
+    }
+    resultEle.innerHTML = formatted;
+  }, [result]);
+  const handleClick = (e) => {
+    if (e.target.id === "reset") {
+      setResult("0");
+    } else if (e.target.id === "del") {
+      if (result.length === 1) {
+        setResult("0");
+      } else {
+        setResult(result.slice(0, -1));
+      }
+    } else if (e.target.id === "equal") {
+      setResult(eval(result)+"");
+    } else if (result.length > 12) {
+      alert("Max length exceeded ! ");
+      return;
+    }
+    else if (result === "0") {
+      setResult(e.target.id);
+    } else if (e.target.id === "+" || e.target.id === "-" || e.target.id === "*" || e.target.id === "/" || e.target.id === ".") {
+      if (result[result.length-1] === "+" || result[result.length-1] === "-" || result[result.length-1] === "*" || result[result.length-1] === "/" || result[result.length-1] === ".") {
+        setResult(result.slice(0, -1) + e.target.id);
+      } else {
+        setResult(result + e.target.id);
+      }
+    }
+    else {
+      setResult(result + e.target.id);
+    }
+    console.log(e.target.id);
+  }
+
+  const buttonslist = [7,8,9,"del",4,5,6,"+",1,2,3,"-",".",0,"/","*","reset","="];
+  const buttons = buttonslist.map((button) => {
+    if (button === "=") {
+      return <button onClick={handleClick} key={button} id={"equal"} className="equal-button">{button}</button>
+    }
+    return <button onClick={handleClick} key={button} id={button}>{button}</button>
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Calc</h1>
+        <div className="theme-div">
+          <label>Theme</label>
+          <div className="change-theme-div">
+            <p><span>1</span><span>2</span><span>3</span></p>
+            <div className="indicator-div">
+              <div className="inner-indicator">
+                <div id="1" className="active-theme" onClick={changeTheme}></div>
+                <div id="2" onClick={changeTheme}></div>
+                <div id="3" onClick={changeTheme}></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
+      <div className="screen">
+        <div className="result-div">
+          <span className="result">0</span>
+        </div>
+      </div>
+      <div className="dial-pad">
+        {buttons}
+      </div>
     </div>
   );
 }
